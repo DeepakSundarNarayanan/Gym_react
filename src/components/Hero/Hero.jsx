aee06b7299c0d8b3fde03604d6a921f5d8a72a42
Deepak Sundar Narayanan <narayanan.dee@northeastern.edu>
2024-05-09 12:58:44 +0530

    commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Hero.css'
+import Header from '../Header/Header'
+
+const Hero = () => {
+  return (
+    <div className="Hero">
+
+        <div className="left-h">
+            <Header />
+      
+        </div>
+        <div className="left-r">right side</div>
+        
+
+
+
+    </div>
+    
+  )
+}
+
+export default Hero
